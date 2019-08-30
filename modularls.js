0e6d672c68964c333c1aa8e2afa0834c76652ad9
Nico Campos <44167340+flustik@users.noreply.github.com>
2019-08-31 05:14:16 +0800

    Update modularls.js

--- modularls.js
+++ modularls.js
@@ -1,10 +1,14 @@
-var lslib = require('./lslib');
-
-var dirname = process.argv[2];
-var ext = process.argv[3];
-
-lslib(dirname, ext, function(err, files) {
-  for (i = 0; i < files.length; i++) {
-    console.log(files[i]);
-  }
-});
+var fs = require('fs')
+var path = require('path')
+ 
+var folder = process.argv[2]
+var ext = '.' + process.argv[3]
+ 
+fs.readdir(folder, function (err, files) {
+  if (err) return console.error(err)
+  files.forEach(function(file) {
+      if (path.extname(file) === ext) {
+          console.log(file)
+      }
+  })
+})
