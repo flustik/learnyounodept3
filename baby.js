980b11a656a3a8b8501b46f29aaa0795111f53ec
flustik <slugraiper@gmail.com>
2019-08-30 21:17:23 +0800

    first commit

---
+++ baby.js
@@ -0,0 +1,8 @@
+const numbers = process.argv.splice(2);
+
+const sum = numbers.reduce((previousValue, currentValue) => {
+    return previousValue + parseInt(currentValue);
+}, 0);
+
+console.log(sum);
+ 
